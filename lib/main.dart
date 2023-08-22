9105419618142a9fe5f145dfd5e190ab97974894
Leandro Heidemann <leandro.heidemann@outlook.com>
2023-08-21 21:08:06 -0300

    Image

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   containerBuild(
-      {Color bgColor = Colors.yellow,
-      double w = 50,
-      double h = 50,
+      {Color bgColor = Colors.yellowAccent,
+      double w = 100,
+      double h = 100,
       double radius = 0}) {
     return Container(
-      margin: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(5),
       width: w,
       height: h,
       decoration: BoxDecoration(
@@ -28,19 +28,36 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    var imageFit = BoxFit.cover;
+
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(title: const Text("AppBar")),
-          body: OrientationBuilder(builder: (context, orientation) {
-            return orientation == Orientation.portrait
-                ? containerBuild(
-                    w: double.infinity, h: double.infinity, bgColor: Colors.red)
-                : containerBuild(
-                    w: double.infinity,
-                    h: double.infinity,
-                    bgColor: Colors.green);
-          }),
-        ));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Imagem 200 X 200")),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const Text('250 X 250'),
+          Container(
+            width: 250,
+            height: 250,
+            color: Colors.black12,
+            child: Image.asset('assets/images/flash.jpg', fit: imageFit),
+          ),
+          const Text('300 X 100'),
+          Container(
+            width: 300,
+            height: 100,
+            color: Colors.black12,
+            child: Image.asset('assets/images/flash.jpg', fit: imageFit),
+          ),
+          const Text('100 X 250'),
+          Container(
+            width: 100,
+            height: 250,
+            color: Colors.black12,
+            child: Image.asset('assets/images/flash.jpg', fit: imageFit),
+          ),
+        ]),
+      ),
+    );
   }
 }
